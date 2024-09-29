67a337b730f430f44d58ce6b16905d0f7ad635c7
MRSIMM0 <s.pasieczny02@gmail.com>
2024-09-29 05:23:58 +0200

    Added landing page

--- pathfinder-ui/app/page.tsx
+++ pathfinder-ui/app/page.tsx
@@ -1,11 +1,40 @@
+"use client"
+import { ThreeDots } from 'react-loader-spinner';
 import styles from './page.module.css';
+import Image from 'next/image';
+import { position } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      window.location.href = '/map';
+    }, 2000);
+
+  }, []);
+
+
   return (
-    <div className={styles.page}>
       <main className={styles.main}>
-        <h2>Hello World!</h2>
+      <div className={styles.img}>
+        <Image alt='' src={'/svg/logo.svg'} fill />
+      </div>
+        <p className={styles.title}>
+        Safe Cycler
+        </p>
+        <div className={styles.gradient}>
+          <Image alt='' src={'/svg/gradient.svg'} fill />
+        </div>
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="white"
+          radius="9"
+          wrapperStyle={{position: 'absolute', bottom: '130px'}}
+          />
       </main>
-    </div>
   );
 }
